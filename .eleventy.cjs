1ce8e7141a9809190c96a15ca4fd8627ccdddf31
Lea Verou <lea@verou.me>
2023-04-30 00:48:32 -0400

    Add 11ty and postcss config, basic post listing

---
+++ .eleventy.cjs
@@ -0,0 +1,73 @@
+let markdownIt = require("markdown-it");
+let markdownItAnchor = require("markdown-it-anchor");
+let markdownItAttrs = require('markdown-it-attrs');
+
+module.exports = config => {
+	let data = {
+		layout: "page.njk",
+		permalink: "{{ page.filePathStem }}.html",
+	};
+
+	for (let p in data) {
+		config.addGlobalData(p, data[p]);
+	}
+
+	config.setDataDeepMerge(true);
+
+	config.setFrontMatterParsingOptions({
+		excerpt: true,
+		// Optional, default is "---"
+		excerpt_separator: "<!-- more -->"
+	});
+
+	let md = markdownIt({
+		html: true,
+		linkify: true,
+	})
+	.disable("code");
+
+	config.setLibrary("md", markdownIt({
+			html: true,
+		})
+		.use(markdownItAttrs)
+		.use(markdownItAnchor, {
+			permalink: markdownItAnchor.permalink.headerLink(),
+			level: 2,
+		})
+		.disable("code")
+	);
+
+	config.addFilter("md", (value) => {
+		if (typeof value !== "string") {
+			return value;
+		}
+
+		return md.render(value);
+	});
+
+	config.addFilter("md_inline", (value) => {
+		if (typeof value !== "string") {
+			return value;
+		}
+
+		return md.renderInline(value);
+	});
+
+	config.addFilter(
+		"relative",
+		page => {
+			let path = page.url.replace(/[^/]+$/, "");
+			let ret = require("path").relative(path, "/");
+
+			return ret || ".";
+		}
+	);
+
+	return {
+		markdownTemplateEngine: "njk",
+		templateFormats: ["md", "njk"],
+		dir: {
+			output: "."
+		},
+	};
+};
