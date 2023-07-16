8520e9d97bebe20898dd298d944c8ce2ecf6e071
Lea Verou <lea@verou.me>
2023-07-16 19:29:19 +0300

    Better tag formatting

--- assets/filters.cjs
+++ assets/filters.cjs
@@ -1,6 +1,8 @@
 const tagNames = require("../_data/tag_names.json");
+const capitalizations = require("../_data/capitalizations.json");
+const fakeTags = new Set(["blog", "all"]);
 
-module.exports = {
+const filters = {
 	relative(page) {
 		let path = page.url.replace(/[^/]+$/, "");
 		let ret = require("path").relative(path, "/");
@@ -15,6 +17,41 @@ module.exports = {
 	},
 
 	format_tag(tag) {
-		return tagNames[tag] ?? tag;
-	}
+		if (tag in tagNames) {
+			return tagNames[tag];
+		}
+
+		tag = tag.replace(/-/g, " ");
+
+		for (let capitalization of capitalizations) {
+			let lowercase = capitalization.toLowerCase();
+			let regex = new RegExp(`\\b${lowercase}(?=\\b|\\d)`, "gi");
+			tag = tag.replace(regex, capitalization);
+		}
+
+		return tag.replace(/\b\w/g, l => l.toUpperCase());
+	},
+
+	is_real_tag (tag) {
+		return !fakeTags.has(tag);
+	},
+
+	real_tags_only (tags) {
+		return tags.filter(filters.is_real_tag);
+	},
+
+	taglist (collections) {
+		let tags = Object.keys(collections).filter(filters.is_real_tag);
+		tags.sort((a, b) => collections[b].length - collections[a].length);
+
+		return Object.fromEntries(tags.map(tag => [tag, collections[tag].length]));
+	},
+
+	// Console.log() arguments, for debugging
+	log(...args) {
+		console.log(...args);
+		return args[0];
+	},
 }
+
+module.exports = filters;
