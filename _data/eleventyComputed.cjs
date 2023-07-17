7dcc8ed2fa08c312d185a7364e01dd08e0f08c0f
Lea Verou <lea@verou.me>
2023-07-17 14:59:08 +0300

    Footer-specific nav links, add link to repo

--- _data/eleventyComputed.cjs
+++ _data/eleventyComputed.cjs
@@ -1,5 +1,11 @@
 
 module.exports = {
+	oldURL (data) {
+		let {site, page} = data;
+		let path = page.url.replace("/blog/", "/").replace("/tag/", "/tags/");
+		return site.old_domain + path;
+	},
+
 	siteArea (data) {
 		return data.page.url.match(/^\/?([^/]+)/)?.[1] ?? "";
 	}
