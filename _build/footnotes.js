ab6dbf99a9ea916d1e68cc4fe45147470e2ce28d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-11-14 17:15:04 +0100

    First stab at improving footnotes a11y (#128)

--- _build/footnotes.js
+++ _build/footnotes.js
@@ -3,9 +3,10 @@ import matchHelper from "posthtml-match-helper";
 /**
  * PostHTML plugin to add a footnote text right after its reference.
  * The text is added as a `<span>` element with the "footnote-text" class right after the footnote reference.
- * Both the footnote reference and the text are enclosed in a container element (`span.footnote-container`) to allow positioning footnote text in a popup next to the ref.
- * Every footnote reference becomes a position anchor for the corresponding footnote. 
- * The list of all footnotes in a blog post is preserved.
+ * Both the footnote reference and the text are enclosed in a container element (`span.footnote-container`) to allow positioning footnote text in a pop-up next to the ref.
+ * Every footnote reference becomes a position anchor for the corresponding footnote.
+ * The list of all footnotes in a blog post is preserved but wrapped in a `<details>` element with the footnote count as a summary.
+ * Improves the overall a11y by adding appropriate ARIA roles and labels.
  *
  * @param {Record<string, any>} [options]
  * @returns {Function} PostHTML plugin function
@@ -15,6 +16,57 @@ export default function footnotesPostHTMLPlugin (options = {}) {
 		// One per page
 		let footnotes = new Map(); // id -> content
 		let processedNodes = new Set();
+
+		tree.match(matchHelper(".footnotes-list"), function (node) {
+			if (processedNodes.has(node)) {
+				// Skip
+				return node;
+			}
+
+			// Add ARIA role to the footnotes list
+			node.attrs.role = "doc-endnotes";
+
+			let footnoteNodes = node.content.filter((n) => n.tag === "li");
+			let footnoteCount = footnoteNodes.length;
+
+			// Add ARIA role to each footnote
+			for (let footnote of footnoteNodes) {
+				footnote.attrs.role = "doc-footnote";
+
+				// Add ARIA role to backref link
+				let content = footnote.content;
+				while (content) {
+					let anchor = content.find(
+						(n) =>
+							n.tag === "a" && n.attrs?.class?.includes("footnote-backref"),
+					);
+
+					if (anchor) {
+						anchor.attrs.role = "doc-backlink";
+						anchor.attrs["aria-label"] = "Back to content";
+						break;
+					}
+					else {
+						// Dive deeper
+						content = content.find((n) => Array.isArray(n.content))?.content;
+					}
+				}
+			}
+
+			processedNodes.add(node);
+
+			// Wrap footnotes list in a <details> element
+			return {
+				tag: "details",
+				content: [
+					{
+						tag: "summary",
+						content: `${footnoteCount}&nbsp;${footnoteCount > 1 ? "footnotes" : "footnote"}`,
+					},
+					node,
+				],
+			};
+		});
 
 		tree.match(matchHelper(".footnote-item"), function (node) {
 			// Clone content to avoid modifying the original node (we should keep it as-is in the footnotes list)
